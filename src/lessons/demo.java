0859b10a0cf03f1b40ce71d7e1bd48125bc4905a
Kacper <panhostick@gmail.com>
2023-08-03 15:26:39 +0200

    Zadanie 2

--- src/lessons/demo.java
+++ src/lessons/demo.java
@@ -3,7 +3,16 @@ package lessons;
 public class demo {
     public static void main(String[] args) {
 
-        User kacper = new User("Kacper", "Tester", "kacper.pindelski@cloudimed.pl", 26);
-        kacper.getFullInfo();
+//        User kacper = new User("Kacper", "Tester", "kacper.pindelski@cloudimed.pl", 26);
+//        kacper.greetings(kacper.firstName);
+//        kacper.getFullInfo();
+
+        Bug testBug = new Bug("Nie działa aplikacja", "kacper.pindelski@cloudimed.pl",5);
+        testBug.bugStatus = false;
+        testBug.bugAllInformation();
+        testBug.personReportBug();
+        testBug.getBugStatus();
+        int bugPriority = testBug.getBugPriority();
+        System.out.println("Priorytet błędu to: " + bugPriority);
     }
 }
